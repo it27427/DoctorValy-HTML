102feb8272afe46f3e6ff3409da651aae701da1e
it27427 <istiak.tushar.qtec@gmail.com>
2023-05-19 17:08:31 +0600

    feedback page completed

--- assets/js/validation/validation.js
+++ assets/js/validation/validation.js
@@ -8,6 +8,7 @@
   // Loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
+      event.preventDefault()
       if (!form.checkValidity()) {
         event.preventDefault()
         event.stopPropagation()
