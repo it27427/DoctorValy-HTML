bfbe177d8eb7cdcd10c3743534d76a8a5ef965f8
it27427 <istiak.tushar.qtec@gmail.com>
2023-05-20 13:33:19 +0600

    PROFESSIONAL-INFORMATION completed

--- assets/js/showhide/showhide.js
+++ assets/js/showhide/showhide.js
@@ -1,8 +1,10 @@
 function showHideSection() {
-  const educationInfo = document.querySelector('.educationinfoform');
+  const educationform = document.querySelector('#educationform');
+  const experienceform = document.querySelector('#experienceform');
   const parent = this.parentElement;
   parent.classList.add('d-none');
-  educationInfo.classList.remove('d-none');
+  educationform.classList.remove('d-none');
+  experienceform.classList.remove('d-none');
 }
 
 const btnQualifications = document.querySelectorAll('.btn-qualifications');
