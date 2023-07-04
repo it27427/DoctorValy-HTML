d66a398de2ba48452a6d2399fb15e73aa5e0434a
it27427 <istiak.tushar.qtec@gmail.com>
2023-07-04 17:40:18 +0600

    chamberlist, create-doctor file updated

---
+++ assets/js/nice-select/doctorselect.js
@@ -0,0 +1,24 @@
+NiceSelect.bind(document.getElementById("divisions"), {
+  searchable: false,
+  placeholder: 'Select Division'
+});
+
+NiceSelect.bind(document.getElementById("districts"), {
+  searchable: true,
+  placeholder: 'Select District'
+});
+
+NiceSelect.bind(document.getElementById("thanas"), {
+  searchable: true,
+  placeholder: 'Select Thana'
+});
+
+NiceSelect.bind(document.getElementById("normal-select-1"), {
+  searchable: false,
+  placeholder: 'Select Degree'
+});
+
+NiceSelect.bind(document.getElementById("selectinstitute"), {
+  searchable: true,
+  placeholder: 'Select Institute'
+});
